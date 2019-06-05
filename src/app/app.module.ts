da85f4f9c77ef239ee4093a98c00301c25cb61d7
UnsungGhost <randypweyer@gmail.com>
2019-06-05 12:46:05 -0700

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
+import { allAppRoutes } from './routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(allAppRoutes),
     AppRoutingModule
   ],
   providers: [],
